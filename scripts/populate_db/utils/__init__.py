ef31f7f4fe0984586ebaf54780e7fd1224a772b2
degenarek <derek@gena.co>
2025-02-03 17:24:39 +0900

    move db code to data-synth repo

--- scripts/populate_db/utils/__init__.py
+++
@@ -1,5 +0,0 @@
-from .arg_parser import parse_args
-from .db_operations import insert_data, truncate_table
-from .get_dependencies import DatabaseValuesExtractor, get_table_dependencies
-from .prompt_formatter import format_prompt
-from .settings import Config
